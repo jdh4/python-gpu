3ea1df327c9617782a2ff3e5d18f71371bf508ec
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-10-16 23:25:10 -0400

    Create example.py

---
+++ jax/example.py
@@ -0,0 +1,16 @@
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+
+def predict(params, inputs):
+  for W, b in params:
+    outputs = jnp.dot(inputs, W) + b
+    inputs = jnp.tanh(outputs)  # inputs to the next layer
+  return outputs                # no activation on last layer
+
+def loss(params, inputs, targets):
+  preds = predict(params, inputs)
+  return jnp.sum((preds - targets)**2)
+
+grad_loss = jit(grad(loss))  # compiled gradient evaluation function
+perex_grads = jit(vmap(grad_loss, in_axes=(None, 0, 0)))  # fast per-example grads
+print("completed")
