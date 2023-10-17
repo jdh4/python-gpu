0cc1a745e370de935385fc79550dc544a532ac34
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-10-16 22:47:20 -0400

    Update and rename svd_numpy.py to matmul_numpy.py

--- cupy/matmul_numpy.py
+++ cupy/matmul_numpy.py
@@ -1,14 +1,15 @@
 from time import perf_counter
 import numpy as np
 
-N = 3000
+N = 10000
+np.random.seed(42)
 X = np.random.randn(N, N).astype(np.float64)
 
 trials = 3
 times = []
 for _ in range(trials):
     t0 = perf_counter()
-    u, s, v = np.linalg.svd(X)
+    Y = np.matmul(X, X)
     times.append(perf_counter() - t0)
 print("Execution time: ", min(times))
-print("sum(s) / N = ", s.sum() / N)
+print("sum(Y) / N = ", Y.sum() / N)
