fd4c797905c19d2ca05fbea06af494a0860f8312
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-10-16 22:43:18 -0400

    Update and rename svd_cupy.py to matmul_cupy.py

--- cupy/matmul_cupy.py
+++ cupy/matmul_cupy.py
@@ -1,15 +1,19 @@
 from time import perf_counter
+import numpy as np
 import cupy as cp
 
-N = 3000
-X = cp.random.randn(N, N, dtype=cp.float64)
+# generate the same matrix as in the numpy case
+N = 10000
+np.random.seed(42)
+X = np.random.randn(N, N).astype(np.float64)
+X = cp.asarray(X)
 
 trials = 3
 times = []
 for _ in range(trials):
     t0 = perf_counter()
-    u, s, v = cp.linalg.svd(X)
+    Y = cp.matmul(X, X)
     cp.cuda.Device(0).synchronize()
     times.append(perf_counter() - t0)
 print("Execution time: ", min(times))
-print("sum(s) / N = ", s.sum() / N)
+print("sum(Y) / N = ", Y.sum() / N)
